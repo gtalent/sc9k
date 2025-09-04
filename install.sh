754e89da7a221b95f0f2a4bfb2b6a1db46939123
Gary Talent <gary@drinkingtea.net>
2025-09-03 23:00:36 -0500

    Fix .desktop file to work in GNOME

--- install.sh
+++ install.sh
@@ -7,3 +7,7 @@ dist_dir=.
 install -Dm 755 $dist_dir/bin/SlideController /usr/local/bin/SlideController
 install -Dm 755 $dist_dir/share/applications/net.drinkingtea.sc9k.desktop /usr/share/applications/net.drinkingtea.sc9k.desktop
 cp -r $dist_dir/share/ /usr/
+
+if command -v gtk-update-icon-cache >/dev/null 2>&1; then
+	sudo gtk-update-icon-cache /usr/share/icons/hicolor
+fi
