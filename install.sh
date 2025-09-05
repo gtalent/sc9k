3dc9c561a8156bb11eaa2c25e020e2275081a270
Gary Talent <gary@drinkingtea.net>
2025-09-04 21:55:13 -0500

    Fix .desktop file permissions

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ set -eu
 dist_dir=.
 
 install -Dm 755 $dist_dir/bin/SlideController /usr/local/bin/SlideController
-install -Dm 755 $dist_dir/share/applications/net.drinkingtea.sc9k.desktop /usr/share/applications/net.drinkingtea.sc9k.desktop
+install -Dm 644 $dist_dir/share/applications/net.drinkingtea.sc9k.desktop /usr/share/applications/net.drinkingtea.sc9k.desktop
 cp -r $dist_dir/share/ /usr/
 
 if command -v gtk-update-icon-cache >/dev/null 2>&1; then
-	sudo gtk-update-icon-cache /usr/share/icons/hicolor
+	gtk-update-icon-cache /usr/share/icons/hicolor
 fi
